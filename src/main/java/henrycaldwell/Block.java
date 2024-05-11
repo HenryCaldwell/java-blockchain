56623e4aff6021c905dded7d2ab3e27310a4b7a3
Henry Caldwell <henrycaldwell2005@gmail.com>
2024-05-11 17:28:58 -0400

    Added javadoc comments to the Block class

--- src/main/java/henrycaldwell/Block.java
+++ src/main/java/henrycaldwell/Block.java
@@ -3,31 +3,49 @@ package henrycaldwell;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Represents a block in a blockchain with transactions, hashes, and other identifiers.
+ */
 public class Block {
 
-    public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
-	public String previousHash;
-	private long timeStamp;
-	public String hash;
-	private int nonce;
-	public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); // The list of transactions in the block.
+	public String hash, previousHash; // The hash and previous hash of the previous block.
+	private long timeStamp;	// The time of the blocks creation in miliseconds.
+	private int nonce; // The nonce value used for mining the block.
+	public String merkleRoot; // The merkle root hash of all the transactions in the block.
 
+	/**
+     * Constructs a Block instance by initializing hashes and timeStamp.
+	 * @param previousHash The hash of the previous block.
+     */
 	public Block(String previousHash) {
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
 		this.hash = calculateHash();
 	}
 
+	/**
+     * Calculates the hash of the block by combining its identifiers and invoking the SHA-256 hashing algorithm.
+     * @return The calculated hash.
+     */
 	public String calculateHash() {
-		String calculatedhash = StringUtil.invokeSha256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + merkleRoot);
+		String calculatedhash = StringUtil.invokeSha256(
+			previousHash + 
+			Long.toString(timeStamp) + 
+			Integer.toString(nonce) + 
+			merkleRoot);
 		return calculatedhash;
 	}
 
+	/**
+     * Mines the block by finding a hash that starts with a specific number of zeroes (the difficulty), creating a 'proof of work' system.
+     * @param difficulty The difficulty level for mining, represented by the number of zeroes that must lead the hash.
+     */
 	public void mineBlock(int difficulty) {
 		merkleRoot = StringUtil.getMerkleRoot(transactions);
 		String target = new String(new char[difficulty]).replace('\0', '0');
 
-		while(!hash.substring(0, difficulty).equals(target)) {
+		while (!hash.substring(0, difficulty).equals(target)) {
 			nonce++;
 			hash = calculateHash();
 		}
@@ -35,18 +53,23 @@ public class Block {
 		System.out.println("*Block Mined* HASH: " + hash);
 	}
 
+	/**
+     * Adds a transaction to the block after verifying it.
+     * @param transaction The transaction to be added to the block.
+     * @return True if the transaction was added successfully, false otherwise.
+     */
 	public boolean addTransaction(Transaction transaction) {
-		if (transaction == null) return false;
+		if (transaction == null) {
+			return false;
+		}
 
-		if ((previousHash != "0")) {
+		if ((previousHash != "0000000000000000000000000000000000000000000000000000000000000000")) {
 			if ((transaction.processTransaction() != true)) {
-				System.out.println("Transaction failed to process. Discarded.");
 				return false;
 			}
 		}
 
 		transactions.add(transaction);
-		System.out.println("Transaction successfully added to block");
 		return true;
 	}
 }
